246050c51832f88f028c1fedd4d049c3baac82a3
mateo-cecchi-davinci <89209029+mateo-cecchi-davinci@users.noreply.github.com>
2024-05-18 17:32:30 -0300

    feature/many_to_many

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mysql from "mysql2";
 import cors from "cors";
 import { createRequire } from "module";
+import { resolve } from "path";
 
 const require = createRequire(import.meta.url);
 const app = express();
@@ -84,7 +85,7 @@ app.listen(8800, () => {
 // });
 
 app.get("/user", authenticateToken, (req, res) => {
-  const query = "SELECT * FROM users WHERE email = ?";
+  const query = "SELECT 1 FROM users WHERE email = ?";
   db.query(query, [user.email], (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
@@ -117,7 +118,7 @@ app.post("/register", async (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-  const query = "SELECT * FROM users WHERE email = ?";
+  const query = "SELECT 1 FROM users WHERE email = ?";
   db.query(query, [req.body.email], async (err, data) => {
     if (err) return res.json(err);
     if (data == null) {
@@ -163,7 +164,7 @@ function generateAccessToken(user) {
 }
 
 app.post("/token", (req, res) => {
-  const query = "SELECT * FROM users WHERE refresh_token = ?";
+  const query = "SELECT 1 FROM users WHERE refresh_token = ?";
   const refreshToken = req.body.token;
 
   if (refreshToken == null) return res.sendStatus(401);
@@ -196,19 +197,87 @@ app.get("/buisnesses", (req, res) => {
 });
 
 app.post("/buisnesses", (req, res) => {
-  const query =
-    "INSERT INTO buisnesses (`name`, `logo`, `street`, `number`, `category`, `fk_buisnesses_users`) VALUES (?)";
-  const values = [
-    req.body.name,
-    req.body.logo,
-    req.body.street,
-    req.body.number,
-    req.body.category,
-    req.body.fk_buisnesses_users,
-  ];
-  db.query(query, [values], (err, data) => {
+  const {
+    name,
+    logo,
+    street,
+    number,
+    category,
+    fk_buisnesses_users,
+    products,
+  } = req.body;
+
+  if (
+    !name ||
+    !logo ||
+    !street ||
+    !number ||
+    !category ||
+    !fk_buisnesses_users ||
+    !Array.isArray(products)
+  ) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  const userCheckQuery = "SELECT 1 FROM users WHERE user_id = ?";
+  db.query(userCheckQuery, [fk_buisnesses_users], (err, result) => {
     if (err) return res.json(err);
-    return res.json("Buisness has been created successfully");
+    if (result.length === 0) return res.json("User not found");
+
+    db.beginTransaction((err) => {
+      if (err) return res.json(err);
+
+      const buisness_query =
+        "INSERT INTO buisnesses (`name`, `logo`, `street`, `number`, `category`, `fk_buisnesses_users`) VALUES (?)";
+      const buisness_values = [
+        name,
+        logo,
+        street,
+        number,
+        category,
+        fk_buisnesses_users,
+      ];
+
+      db.query(buisness_query, [buisness_values], (err, data) => {
+        if (err)
+          return db.rollback(() => {
+            res.json(err);
+          });
+
+        const inserted_buisness_id = data.insertId;
+        const products_queries = products.map((product) => {
+          const [product_id, stock] = product;
+          return new Promise((resolve, reject) => {
+            const productQuery =
+              "INSERT INTO buisnesses_products (`fk_buisnesses_products_buisnesses`, `fk_buisnesses_products_products`, `stock`) VALUES (?, ?, ?)";
+            db.query(
+              productQuery,
+              [inserted_buisness_id, product_id, stock],
+              (err, result) => {
+                if (err) return reject(err);
+                resolve(result);
+              }
+            );
+          });
+        });
+
+        Promise.all(products_queries)
+          .then(() => {
+            db.commit((err) => {
+              if (err)
+                return db.rollback(() => {
+                  res.json("Transaction commit error");
+                });
+              res.json("Business created and products related successfully");
+            });
+          })
+          .catch((err) => {
+            db.rollback(() => {
+              res.json("Failed to insert products for business: " + err);
+            });
+          });
+      });
+    });
   });
 });
 
@@ -293,23 +362,71 @@ app.get("/users", (req, res) => {
 
 app.post("/users", async (req, res) => {
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const values = [
-      req.body.name,
-      req.body.lastname,
-      req.body.email,
+    const { name, lastname, email, password, is_manager } = req.body;
+
+    if (!name || !lastname || !email || !password) {
+      return res.json("All fields are required");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user_values = [
+      name,
+      lastname,
+      email,
       hashedPassword,
       0,
       0,
       0,
-      req.body.is_manager,
+      is_manager,
     ];
-    const query =
-      "INSERT INTO users (`name`, `lastname`, `email`, `password`, `failed_attempts`, `blocked`, `is_admin`, `is_manager`) VALUES (?)";
 
-    db.query(query, [values], (err, data) => {
+    db.beginTransaction((err) => {
       if (err) return res.json(err);
-      return res.json("User has been created successfully");
+
+      const user_query =
+        "INSERT INTO users (`name`, `lastname`, `email`, `password`, `failed_attempts`, `blocked`, `is_admin`, `is_manager`) VALUES (?)";
+
+      db.query(user_query, [user_values], (err, data) => {
+        if (err) return res.json(err);
+
+        const inserted_user_id = data.insertId;
+
+        const cupons_query = "SELECT cupon_id FROM cupons";
+        db.query(cupons_query, (err, cupons) => {
+          if (err) return res.json(err);
+
+          const cupons_queries = cupons.map((cupon) => {
+            return new Promise((resolve, reject) => {
+              const cupons_users_query =
+                "INSERT INTO cupons_users (`fk_cupons_users_users`, `fk_cupons_users_cupons`, `state`) VALUES (?, ?, ?)";
+              db.query(
+                cupons_users_query,
+                [inserted_user_id, cupon.cupon_id, 0],
+                (err, res) => {
+                  if (err) return reject(err);
+                  resolve(res);
+                }
+              );
+            });
+          });
+
+          Promise.all(cupons_queries)
+            .then(() => {
+              db.commit((err) => {
+                if (err)
+                  return db.rollback(() => {
+                    res.json("Transaction commit error");
+                  });
+                res.json("User created successfully");
+              });
+            })
+            .catch((err) => {
+              db.rollback(() => {
+                res.json("Failed to insert cupons for users: " + err);
+              });
+            });
+        });
+      });
     });
   } catch {
     res.status(500).send();
@@ -360,12 +477,55 @@ app.get("/orders", (req, res) => {
 });
 
 app.post("/orders", (req, res) => {
-  const query =
-    "INSERT INTO orders (`code`, `state`, `fk_orders_users`) VALUES (?)";
-  const values = [req.body.code, req.body.state, req.body.fk_orders_users];
-  db.query(query, [values], (err, data) => {
+  const { code, fk_orders_users, products } = req.body;
+
+  if (!code || !fk_orders_users || !Array.isArray(products)) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  db.beginTransaction((err) => {
     if (err) return res.json(err);
-    return res.json("Order has been created successfully");
+
+    const order_query =
+      "INSERT INTO orders (`code`, `state`, `fk_orders_users`) VALUES (?)";
+    const order_values = [code, "Pendent", fk_orders_users];
+    db.query(order_query, [order_values], (err, data) => {
+      if (err) return res.json(err);
+
+      const inserted_order_id = data.insertId;
+
+      const products_queries = products.map((product) => {
+        const [product_id, amount] = product;
+        return new Promise((resolve, reject) => {
+          const product_query =
+            "INSERT INTO orders_products (`fk_orders_products_orders`, `fk_orders_products_products`, `amount`) VALUES (?, ?, ?)";
+          db.query(
+            product_query,
+            [inserted_order_id, product_id, amount],
+            (err, res) => {
+              if (err) return reject(err);
+              resolve(res);
+            }
+          );
+        });
+      });
+
+      Promise.all(products_queries)
+        .then(() => {
+          db.commit((err) => {
+            if (err)
+              return db.rollback(() => {
+                res.json("Transaction commit error");
+              });
+            res.json("Order created successfully");
+          });
+        })
+        .catch((err) => {
+          db.rollback(() => {
+            res.json("Failed to insert products for orders: " + err);
+          });
+        });
+    });
   });
 });
 
